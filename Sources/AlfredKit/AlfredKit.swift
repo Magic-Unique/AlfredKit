f0637ea3c880813b9cf29e85731b41d68e9c8b7c
uniquewu <uniquewu@tencent.com>
2021-04-22 21:22:01 +0800

    Support other fields

--- Sources/AlfredKit/AlfredKit.swift
+++ Sources/AlfredKit/AlfredKit.swift
@@ -4,7 +4,7 @@ import Foundation
 public typealias ScriptFilterJSON = [String: Any]
 public typealias ScriptFilterXMLElement = XMLElement
 
-public protocol ScriptFilterElement {
+protocol ScriptFilterElement {
     
     var json: ScriptFilterJSON { get }
     
@@ -28,6 +28,11 @@ public struct ScriptFilter {
         public var uid: String?
         public var title: String?
         public var subtitle: String?
+        public var arg: String?
+        public var valid: Bool?
+        public var autocomplete: String?
+        public var match: String?
+        public var quicklookurl: String?
         
         public enum Icon {
             case path(String)
@@ -35,6 +40,49 @@ public struct ScriptFilter {
             case type(String)
         }
         public var icon: Icon?
+        
+        public enum RowType {
+            case `default`
+            case file(Bool)
+            
+            var name: String {
+                if case let .file(skipcheck) = self {
+                    return skipcheck ? "file:skipcheck" : "file"
+                } else {
+                    return "default"
+                }
+            }
+        }
+        public var type: RowType?
+        
+        public struct Text {
+            public var copy: String?
+            public var largetype: String?
+            public var enable: Bool { copy != nil || largetype != nil }
+            public var copyElement: ScriptFilterXMLElement? { element(type: "copy", text: copy) }
+            public var largetypeElement: ScriptFilterXMLElement? { element(type: "largetype", text: largetype) }
+            func element(type: String, text: String?) -> ScriptFilterXMLElement? {
+                let element = ScriptFilterXMLElement(name: "text", value: text)
+                element?.setAttributesWith(["type": type])
+                return element
+            }
+        }
+        public var text = Text()
+        
+        public enum ModifierKey: String {
+            case alt, shift, cmd, ctrl, fn
+        }
+        public struct Mod {
+            public var key: ModifierKey
+            public var valid: Bool = true
+            public var subtitle: String?
+            public var arg: String?
+        }
+        public var mods: [ModifierKey: Mod]?
+        public mutating func onPress(_ key: ModifierKey, subtitle: String, arg: String? = nil, valid: Bool = true) {
+            if mods == nil { mods = [:] }
+            mods![key] = Mod(key: key, valid: valid, subtitle: subtitle, arg: arg)
+        }
     }
     
     private var _rows = [Row]()
@@ -58,7 +106,7 @@ public struct ScriptFilter {
 
 extension ScriptFilter: ScriptFilterElement {
     
-    public var json: ScriptFilterJSON {
+    var json: ScriptFilterJSON {
         var json = ScriptFilterJSON()
         json["variables"] = self.variables.count > 0 ? self.variables : nil;
         json["items"] = { () -> [Any] in
@@ -72,21 +120,21 @@ extension ScriptFilter: ScriptFilterElement {
         return json
     }
     
-    public var xmlElement: XMLElement {
+    var xmlElement: XMLElement {
         let element = XMLElement(name: "output")
         if rerun > 0 {
             let _rerun = XMLElement(name: "rerun", value: "\(rerun)")
             element.add(child: _rerun)
         }
-//        if (self.variables.count) {
-//            NSXMLElement *element = [NSXMLElement elementWithName:@"variables"];
-//            [self.variables enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *obj, BOOL * _Nonnull stop) {
-//                NSXMLElement *subelement = [NSXMLElement elementWithName:@"variable" stringValue:obj];
-//                [subelement setAttributesWithDictionary:@{@"name":key}];
-//                [element addChild:subelement];
-//            }];
-//            [root addChild:element];
-//        }
+        if self.variables.count > 0 {
+            let variables = ScriptFilterXMLElement(name: "variables")
+            for (key, value) in self.variables {
+                let node = ScriptFilterXMLElement(name: "variable", stringValue: value)
+                node.setAttributesWith(["name": key])
+                variables.add(child: node)
+            }
+            element.add(child: variables)
+        }
         if self.rows.count > 0 {
             let items = XMLElement(name: "items")
             for row in rows {
@@ -122,61 +170,59 @@ public extension ScriptFilter {
 
 extension ScriptFilter.Row: ScriptFilterElement {
     
-    public var json: ScriptFilterJSON {
+    var json: ScriptFilterJSON {
         var json = ScriptFilterJSON()
-        json["uid"] = self.uid;
-        json["title"] = self.title;
-        json["subtitle"] = self.subtitle;
-//        json["arg"] = self.arg;
-        json["icon"] = self.icon?.json;
-//        json["valid"] = @(self.valid);
-//        json["match"] = self.match;
-//        json["autocomplete"] = self.autocomplete;
-//        json["type"] = AKItemTypeGetName(self.type);
-//        if (self.mMods.count) {
-//            NSMutableDictionary *mods = [NSMutableDictionary dictionary];
-//            for (AKItemModifier *mod in self.mMods.allValues) {
-//                mods[AKModKeyGetName(mod.key)] = mod.JSON;
-//            }
-//            json["mods"] = mods;
-//        }
-//        if (self.onCopyText.text || self.onLargeText.text) {
-//            NSMutableDictionary *text = [NSMutableDictionary dictionary];
-//            text[@"copy"] = self.onCopyText.JSON;
-//            text[@"copy"] = self.onLargeText.JSON;
-//            json["text"] = text;
-//        }
-//        json["quicklookurl"] = self.quicklookurl;
-        
+        json["uid"] = uid
+        json["title"] = title
+        json["subtitle"] = subtitle
+        json["arg"] = arg
+        json["icon"] = icon?.json
+        json["valid"] = valid
+        json["match"] = match
+        json["autocomplete"] = autocomplete
+        json["type"] = type?.name
+        json["quicklookurl"] = quicklookurl
+        if text.enable {
+            var text = [String: String]()
+            text["copy"] = self.text.copy
+            text["largetype"] = self.text.largetype
+            json["text"] = text
+        }
+        if let modCount = mods?.count, modCount > 0 {
+            var mods = ScriptFilterJSON()
+            for (key, mod) in self.mods! {
+                mods[key.rawValue] = mod.json
+            }
+            json["mods"] = mods
+        }
         return json
     }
     
-    public var xmlElement: XMLElement {
+    var xmlElement: XMLElement {
         let item = XMLElement(name: "item")
         
         //  Item Attributes
         var attributes = [String: String]()
         attributes["uid"] = uid;
-//        attributes["arg"] = arg;
-//        attributes["valid"] = AKBOOLGetName(self.valid);
-//        attributes["autocomplete"] = self.autocomplete;
-//        attributes["type"] = AKItemTypeGetName(self.type);
+        attributes["valid"] = valid == nil ? nil : "\(valid!)"
+        attributes["autocomplete"] = autocomplete
+        attributes["type"] = self.type?.name
         item.setAttributesWith(attributes)
         
         //  Item Elements
         item.add(child: XMLElement(name: "title", value: title))
         item.add(child: XMLElement(name: "subtitle", value: subtitle))
-//        for (AKItemSubtitle *subtitle in self.subtitles) {
-//            [item ak_addChild:subtitle.XMLElement];
-//        }
-//        for (AKItemModifier *mod in self.mMods.allValues) {
-//            [item ak_addChild:mod.XMLElement];
-//        }
-//        [item ak_addChild:[NSXMLElement ak_elementWithName:@"arg" stringValue:self.arg]];
-//        [item ak_addChild:self.onCopyText.XMLElement];
-//        [item ak_addChild:self.onLargeText.XMLElement];
-//        [item ak_addChild:[NSXMLElement ak_elementWithName:@"quicklookurl" stringValue:self.quicklookurl]];
+        item.add(child: XMLElement(name: "arg", value: arg))
+        item.add(child: text.copyElement)
+        item.add(child: text.largetypeElement)
+        item.add(child: XMLElement(name: "quicklookurl", value: quicklookurl))
         item.add(child: self.icon?.xmlElement)
+        if let modCount = mods?.count, modCount > 0 {
+            for (_, mod) in self.mods! {
+                item.add(child: mod.xmlElement)
+            }
+        }
+        
         return item;
     }
     
@@ -184,7 +230,7 @@ extension ScriptFilter.Row: ScriptFilterElement {
 
 extension ScriptFilter.Row.Icon: ScriptFilterElement {
     
-    public var json: ScriptFilterJSON {
+    var json: ScriptFilterJSON {
         var json = [String:Any]()
         if case let .path(path) = self {
             json["path"] = path
@@ -200,7 +246,7 @@ extension ScriptFilter.Row.Icon: ScriptFilterElement {
         return json
     }
     
-    public var xmlElement: XMLElement {
+    var xmlElement: ScriptFilterXMLElement {
         let element = XMLElement(name: "icon")
         if case let .path(path) = self {
             element.stringValue = path
@@ -216,6 +262,29 @@ extension ScriptFilter.Row.Icon: ScriptFilterElement {
         return element
     }
     
+}
+
+extension ScriptFilter.Row.Mod: ScriptFilterElement {
+    
+    var json: ScriptFilterJSON {
+        var json = [String:Any]()
+        json["valid"] = valid
+        json["subtitle"] = subtitle
+        json["arg"] = arg
+        return json
+    }
+    
+    var xmlElement: ScriptFilterXMLElement {
+        let element = ScriptFilterXMLElement(name: "mod")
+        // <mod key="shift" subtitle="New subtext when shift is pressed" valid="yes" arg="shiftmod"/>
+        var attributes = [String: String]()
+        attributes["key"] = key.rawValue
+        attributes["subtitle"] = subtitle
+        attributes["valid"] = "\(valid)"
+        attributes["arg"] = arg
+        element.setAttributesWith(attributes)
+        return element
+    }
 }
 
 extension XMLElement {
--- Sources/Demo/main.swift
+++ Sources/Demo/main.swift
@@ -8,22 +8,28 @@
 import Foundation
 import AlfredKit
 
-var scriptFilter = ScriptFilter(xml: false)
+var scriptFilter = ScriptFilter(xml: true)
 scriptFilter.rerun = 1
 scriptFilter.append { (row) in
     row.title = "Title"
     row.subtitle = "Subtitle"
     row.icon = .type("png")
+    row.text.largetype = "largetype"
+    row.text.copy = "copy"
+    row.quicklookurl = "https://www.baidu.com"
 }
 scriptFilter.append { (row) in
     row.title = "Title"
     row.subtitle = "Subtitle"
     row.icon = .type("png")
+    row.onPress(.alt, subtitle: "Press `Alt`")
 }
 scriptFilter.append { (row) in
     row.title = "Title"
     row.subtitle = "Subtitle"
     row.icon = .type("png")
+    row.valid = false
 }
+scriptFilter.variables["aa"] = "bb"
 
 scriptFilter.show()
